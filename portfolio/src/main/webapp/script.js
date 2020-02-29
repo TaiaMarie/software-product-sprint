1e39e16c87a79e32bbae596fc32f4fbcec971c52
Taia <tmwilliams181@students.desu.edu>
2020-02-29 12:53:45 -0500

    modified to fetch json

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,17 +1,3 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 /**
  * Adds a random greeting to the page.
  */
@@ -26,3 +12,25 @@ function addRandomPlace() {
   const placeContainer = document.getElementById('place-container');
   placeContainer.innerText = place;
 }
+function getJSON(){
+    fetch('/data')  // sends a request to /my-data-url
+    .then(response => response.json()) // parses the response as JSON
+    .then((myOb) => { // now we can reference the fields in myOb!
+    const adataListElement = document.getElementById('data-container');
+    myObListElement.innerHTML = '';
+    myObListElement.appendChild(
+        createListElement('Hawaii'));
+    myObListElement.appendChild(
+        createListElement('Kansas'));
+    myObListElement.appendChild(
+        createListElement('Washington'));
+    System.out.print(myOb)
+});
+/** Creates an <li> element containing txt. */
+function createListElement(txt) {
+  const liElement = document.createElement('liw');
+  liElement.innerText = txt;
+  return liElement;
+}
+}
+
