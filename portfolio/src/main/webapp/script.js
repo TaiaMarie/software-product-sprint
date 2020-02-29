b8df124f0958939965382ca935b12aaff76031ef
Taia <tmwilliams181@students.desu.edu>
2020-02-29 18:12:51 -0500

    Was only able to return json string to /data url

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,25 +12,21 @@ function addRandomPlace() {
   const placeContainer = document.getElementById('place-container');
   placeContainer.innerText = place;
 }
-function getJSON(){
-    fetch('/data')  // sends a request to /my-data-url
-    .then(response => response.json()) // parses the response as JSON
-    .then((myOb) => { // now we can reference the fields in myOb!
-    const adataListElement = document.getElementById('data-container');
+function getJStats() {
+  fetch('/data').then(response => response.json()).then((myOb) => {
+    // stats is an object, not a string, so we have to
+    // reference its fields to create HTML content
+
+    const statsListElement = document.getElementById('data-container');
     myObListElement.innerHTML = '';
-    myObListElement.appendChild(
-        createListElement('Hawaii'));
-    myObListElement.appendChild(
-        createListElement('Kansas'));
-    myObListElement.appendChild(
-        createListElement('Washington'));
-    System.out.print(myOb)
-});
-/** Creates an <li> element containing txt. */
-function createListElement(txt) {
-  const liElement = document.createElement('liw');
-  liElement.innerText = txt;
+    console.log('Hawaii');
+    console.log('Kansas');
+    console.log('Washington');
+  });
+  /** Creates an <li> element containing text. */
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
   return liElement;
 }
 }
-
